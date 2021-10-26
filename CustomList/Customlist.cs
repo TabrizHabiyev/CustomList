19e4bde3bf4feff458dc6ae2686229a0ad16080a
TabrizHabiyev <91114214+TabrizHabiyev@users.noreply.github.com>
2021-10-26 06:37:21 +0400

    Create custom generic list

---
+++ CustomList/Customlist.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    class Customlist<T>
+    {
+        private T[] _arr { get; set; }
+        public Customlist(int size = 10)
+        {
+            _arr = new T[size];
+
+        }
+        public T this[int index] 
+        {
+            get 
+            {
+                try
+                {
+                    return _arr[index];
+                }
+                catch
+                {
+
+                    return _arr[0];
+                }
+            }
+            set 
+            {
+                try
+                {
+                    if (index == _arr.Length-1)
+                    {
+                        T[] copy = new T[_arr.Length+10];
+                        _arr = copy;
+                    }
+                    _arr[index] = value;
+                  
+                }
+                catch 
+                {
+                    Console.WriteLine("Out of range");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public int LastIndexOf(T item)
+        {
+            return Array.LastIndexOf(_arr, item);
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_arr, item);
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_arr, 0, _arr.Length);
+        }
+
+        public void Remove(T item)
+        {
+            //int _delete = Array.IndexOf(_arr, item);
+            //if (_delete !<0)
+            //{
+            //    _arr[_delete] = null;
+            //}
+        }
+
+        //Add method
+        public void Add(T item)
+        {
+            for (int i = 0; i < _arr.Length+1; i++)
+            {
+                if (_arr[i] == null)
+                {
+                    _arr[i] = item;
+
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customlist<string> list = new Customlist<string>();
+            list.Add("11");
+            list[1] = "abc";
+            list.Clear();
+            list.IndexOf("11");
+            list.LastIndexOf("11");
+            list.Remove("11");
+            Console.ReadLine();
+        }
+
+    }
+}
